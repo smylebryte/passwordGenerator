bf55ae62f5721b71393174793bd197ec897f4016
Smylebryte <aliciahughes1446@gmail.com>
2021-02-19 22:28:40 -0600

    added code structure

---
+++ script.js
@@ -0,0 +1,67 @@
+// Assignment Code
+let passwordLength = ;
+let uppercase = ["A", "B", "C"]
+let lowercase = ["a", "b", "c"]
+let numeric = ["1", "2", "3"]
+let specialCharacters = ["@", "!", "$"]
+let finalPassword = ""
+
+function generatePassword (
+  passwordLength;
+  uppercase;
+  lowercase;
+  numeric;
+  specialCharacters;
+) {
+
+}
+
+var generateBtn = document.querySelector("#generate");
+
+// User clicks button
+generateBtn.addEventListener("click", writePassword);
+
+// Window prompt asks user what they want for their password
+
+
+// randomizer function
+for (let i=0; i<passwordLength; i++) {
+  finalPassword
+
+
+  return finalPassword
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+// Password output
+
+
+
+
+
+
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+// function generatePassword () {
+
+// }
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
