49c00f0e415f52831a59c773567aeb1092b7d7d7
Smylebryte <aliciahughes1446@gmail.com>
2021-02-21 23:45:53 -0600

    updated javascript

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Assignment Code
+var lowerCase = ["a", "b", "c", "d"];
+var upperCase = ["A", "B", "C", "D"];
+var numeric = [1, 2, 3, 4];
+var specialCharacters = ["!", "@", "#", "$"];
 
+
+
+
+var passwordLength = window.prompt ("Enter password length:");
+var includeLowerCase = window.confirm ("Do you want to include lowercase?");
+var includeUpperCase = window.confirm ("Do you want to include uppercase?");
+var includeNumeric = window.confirm ("Do you want to include numbers?");
+var includeSpecialCharacters = window.confirm ("Do you want to include special characters?");
+
+var characterOptions = [];
 
 var generateBtn = document.querySelector("#generate");
 
@@ -9,26 +23,33 @@ function generatePassword (
     includeLowerCase,
     includeUpperCase,
     includeNumeric
-    ) {
+) {
 
-    let characterOptions = [];
-
-    let lowerCase = ["a", "b", "c", "d"];
-    let upperCase = ["A", "B", "C", "D"];
-    let specialCharacters = ["!", "@", "#", "$"];
-
-    if (includeSpecialCharacters) {
-        characterOptions = characterOptions.concat(specialCharacters);
+    if (includeLowerCase && includeUpperCase && includeNumeric && includeSpecialCharacters) {
+        characterOptions = characterOptions.concat(lowerCase, upperCase, numeric, specialCharacters);
+    } else if 
+        (!includeLowerCase && includeUpperCase && includeNumeric && includeSpecialCharacters) {
+        characterOptions = characterOptions.concat(upperCase, numeric, specialCharacters);
     }
 
-    if (includeUpperCase) {
-        characterOptions = characterOptions.concat(upperCase);
+    else if 
+        (includeLowerCase && !includeUpperCase && includeNumeric && includeSpecialCharacters) {
+        characterOptions = characterOptions.concat(lowerCase, numeric, specialCharacters);
+    }
+    
+    else if 
+        (includeLowerCase && includeUpperCase && !includeNumeric && includeSpecialCharacters) {
+        characterOptions = characterOptions.concat(lowerCase, upperCase, specialCharacters);
     }
 
-    let finalPassword = ""
+    else {
+        characterOptions = characterOptions.concat(lowerCase, uppercase, numeric);
+    }
+
+    var finalPassword = ""
 
     for (i=0; i<passwordLength; i++)  {
-        let randomIndex = Math.floor(Math.random() * characterOptions.length);
+        var randomIndex = Math.floor(Math.random() * characterOptions.length);
         finalPassword += characterOptions[randomIndex];
 
     }
@@ -38,16 +59,17 @@ function generatePassword (
 
 // Write password to the #password input
 function writePassword() {
-    let wasAnythingSelected = false;
-    let passwordLength = window.prompt ("Enter length:");
-    let includeSpecialCharacters = true;
-    let includeLowerCase = true;
-    let includeUpperCase = true;
-    let includeNumeric = true;
+    // let wasAnythingSelected = false;
 
+    // function userChoice () {
+    // let passwordLength = window.prompt ("Enter password length:");
+    // let includeLowerCase = window.confirm ("Do you want to include lowercase?");
+    // let includeUpperCase = window.confirm ("Do you want to include uppercase?");
+    // let includeNumeric = window.confirm ("Do you want to include numbers?");
+    // let includeSpecialCharacters = window.confirm ("Do you want to include special characters?");
+    // }
 
-
-    var password = generatePassword(
+    var password = generatePassword (
         passwordLength,
         includeSpecialCharacters,
         includeLowerCase,
